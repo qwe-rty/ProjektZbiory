18e7a31bbf4e0554104fa0f4822a1362992acaff
qwe-rty <pkurowski@us.edu.pl>
2016-06-03 14:07:15 +0200

    Próba spushowania repozytorium

---
+++ ZbioryEx/src/main/java/IJO_DKK/ZbioryEx/App.java
@@ -0,0 +1,13 @@
+package IJO_DKK.ZbioryEx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
