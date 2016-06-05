a83aee234707cb2228d740714cd5c428d390aea3
kiper <kiper@192.168.0.15>
2016-06-05 15:52:29 +0200

    Testy konstruktorów klasy MyExtendedSet

---
+++ ZbioryEx/src/test/java/setPackage/MESTCtr.java
@@ -0,0 +1,26 @@
+package setPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MESTCtr {
+	
+	@Test
+	public void test1(){
+		MyExtendedSet<Integer> a = new MyExtendedSet<Integer>();
+		assertEquals(a.size(),0);
+		
+	}
+	@Test
+	public void test2(){
+		MyExtendedSet<Integer> a = new MyExtendedSet<Integer>(new Integer[]{2,3});
+		assertEquals(a.size(),2);
+		
+	}
+	@Test
+	public void test3(){
+		MyExtendedSet<Integer> a = new MyExtendedSet<Integer>(1,2,3);
+		assertEquals(a.size(),3);		
+	}
+}
