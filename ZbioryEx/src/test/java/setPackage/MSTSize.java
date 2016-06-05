e315c4545cca876f860039c6a0ac3a76de7d8f47
kiper <kiper@192.168.0.15>
2016-06-05 13:27:51 +0200

    17/30 Test metody size

---
+++ ZbioryEx/src/test/java/setPackage/MSTSize.java
@@ -0,0 +1,42 @@
+package setPackage;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class MSTSize {
+	Integer[] a;
+	int size;
+	boolean expected;
+	
+	@Parameters
+    public static Collection<Object[]> data() {
+    	
+    	return Arrays.asList(new Object[][]
+    			{ 	{ new Integer[]{}, 0, true },
+    				{ new Integer[]{}, 1, false },
+    				{ new Integer[]{0,1,2,3}, 4, true },
+    				{ new Integer[]{0,1,2,3}, 0, false},    				
+    			} );
+    }
+    public MSTSize(Integer[] a, int size, boolean expected){
+		this.a = a;
+		this.size = size;
+		this.expected = expected;
+	}
+    @Test
+	public void test() {
+		
+		MySet<Integer> objA = new MySet<Integer>(a);
+		
+		
+		assertEquals("test size: ", expected, objA.size() == size);
+	}
+}
